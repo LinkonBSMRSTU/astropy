df7618f7daab67d88c71037857c5fca98b06eb2c
Michael Droettboom <mdboom@gmail.com>
2014-01-29 08:17:45 -0500

    Use the bundled argparse on Python 3.1 as well

--- astropy/utils/compat/argparse.py
+++ astropy/utils/compat/argparse.py
@@ -2,7 +2,10 @@ from __future__ import absolute_import
 
 import sys
 
-if sys.version_info[:2] <= (2, 6):
-    from ._argparse_py2 import *
+# argparse is missing on Python 2.6 and 3.1
+major, minor = sys.version_info[:2]
+if ((major == 2 and minor <= 6) or
+    (major == 3 and minor <= 1)):
+    from ._argparse import *
 else:
     from argparse import *
